1cb706672861b7ace1adcf38784562cd9dd009ae
Harsha M Suraweera <harshamanoj912@gmail.com>
2020-08-01 14:06:00 +0530

    second commit

--- app/src/main/java/com/it19208718/ActivityOne/MainActivity.java
+++ app/src/main/java/com/it19208718/ActivityOne/MainActivity.java
@@ -8,21 +8,62 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+
+            TextView txtMgs2 = findViewById(R.id.tvMsg2);
+
+            txtMgs2.setText(R.string.Msg2);
+
+
+            Log.i("lifecycle",  "onCreate() called" );
+
+        }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onStart() {
+        super.onStart();
 
+        Log.i("lifecycle",  "onStart() called" );
+    }
 
-        TextView txtMgs2 = findViewById(R.id.tvMsg2);
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-        txtMgs2.setText(R.string.Msg2);
+        Log.i("lifecycle",  "onResume() called" );
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
 
-        Log.i("lifecycle",  "onCreate() called" );
-
+        Log.i("lifecycle",  "onStop() called" );
     }
 
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i("lifecycle",  "onDestroy() called" );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i("lifecycle",  "onPause() called" );
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.i("lifecycle",  "onRestart() called" );
+    }
 
 }
